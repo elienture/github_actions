9d2dc5557cddb96418d8022d23b9a9b53bc3eb88
Liza Nauman <naumanes@mail.ru>
2024-08-10 19:00:48 +0300

    tests

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import { App } from './app';
 import { Analytic } from './context/analityc';
 import './styles.css';
 
+ 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 root.render(
-	<StrictMode>
-		<Analytic.Provider value={{ yandexId: 96728508, googleId: 'G-ZXWBHB1NR0' }}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</Analytic.Provider>
-	</StrictMode>
+    <StrictMode>
+        <BrowserRouter
+            basename={process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH : '/'}>
+            <App />
+        </BrowserRouter>
+    </StrictMode>
 );
